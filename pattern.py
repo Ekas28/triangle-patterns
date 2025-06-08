a318bfdeff141096fbd6df964009e75c52eb7db4
Ekas28 <125024903+Ekas28@users.noreply.github.com>
2025-06-08 16:24:11 +0530

    patterns.py

--- pattern.py
+++ pattern.py
@@ -1,4 +1,5 @@
 # Lower Triangular
+print("Lower Triangle:")
 n = 5
 for i in range(1, n + 1):
     print("* " * i)
@@ -6,11 +7,13 @@ for i in range(1, n + 1):
 print()
 
 # Upper Triangular
+print("\nUpper Triangle:")
 for i in range(n):
     print("* " * (n - i))
 
 print()
 
 # Pyramid
+print("\nPyramid Pattern:")
 for i in range(1, n + 1):
     print("  " * (n - i) + "* " * (2 * i - 1))
